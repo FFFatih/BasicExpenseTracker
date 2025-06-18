0b819d2a3acb176551ff76d9b41524c5fb646b65
FFFatih <ffatihb6@gmail.com>
2025-06-18 17:06:13 +0300

    eklemeler

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/model/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
@@ -26,7 +27,7 @@ class _ExpensesState extends State<Expenses> {
   _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text("Modal Bottom Sheet"),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
