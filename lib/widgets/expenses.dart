0ab8aad48e0832b551b262c9534e8e7e5473929c
FFFatih <ffatihb6@gmail.com>
2025-07-07 19:42:32 +0300

    save işlevi ve boyutlandırma

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,9 +26,15 @@ class _ExpensesState extends State<Expenses> {
   ];
   _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
