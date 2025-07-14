84a95c5a2f8a5ea78c187188a4c485efca38d3c0
FFFatih <ffatihb6@gmail.com>
2025-07-14 23:18:25 +0300

    tablo için hazırlık

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -36,3 +36,26 @@ class Expense {
   } // tarih formatını gün/ay/yıl olarak döndürüyor
 
 }
+
+class ExpenseBucket{
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+  double get totalExpenses {
+    double sum = 0.0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+    // expenses listesindeki tüm harcamaların toplamını hesaplar
+  }
+}
